aded27a5a6924ab764adf9c48af3b6d79cd4a2b7
GeekyBear <ezegeek@gmail.com>
2023-08-29 15:02:59 -0300

    Secured urls

--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3001/";
-const AUTH_URL = "http://localhost:3001/auth";
+const API_URL = process.env.API_URL;
+const AUTH_URL = process.env.AUTH_URL;
 
 export { API_URL, AUTH_URL };
